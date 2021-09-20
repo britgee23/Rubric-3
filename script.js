ebaa53294d25fe96d61cf09555fcd34cbaab86dc
britgee23 <britgee23@gmail.com>
2021-09-19 21:28:16 -0500

    html

---
+++ script.js
@@ -0,0 +1,8 @@
+var myDemo = document.getElementById("demoparent");
+
+console.log(myDemo.childNodes.length);// JavaScript Document
+
+
+var myDemoTags = document.getElementsByTagName("li");
+
+console.log ("Number of li links: "+ myDemoTags.length);
