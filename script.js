6044c72652b7e49b868625b6bdc3ceeb54ff4465
britgee23 <britgee23@gmail.com>
2021-09-19 22:07:15 -0500

    script

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ console.log(myDemo.childNodes.length);// JavaScript Document
 var myDemoTags = document.getElementsByTagName("li");
 
 console.log ("Number of li links: "+ myDemoTags.length);
+
+var linksInMenu = myDemo.getElementsByTagName("li");
+
+console.log ("Number of li items in Menu: "+ linksInMenu.length);
+
+var myComments = document.getElementById("commentary");
+
+myComments.setAttribute("align", "right");
+
+myComments.setAttribute("style", "color: red");
